f085e727ec008336fae4087fa53d042d4734bf1a
naneps <ekaprasetya2244@gmail.com>
2022-07-03 22:19:38 +0700

    create any method

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <script setup>
 import { ref, computed, onMounted } from 'vue'
 const query = ref('');
-// const my_
+const my_anime = ref([]);
+const search_results = ref([]);
+
+const my_anime_asc = computed(() => {
+  return my_anime.value.sort((a, b) => {
+    return a.title.localeCompare(b.title)
+  })
+})
+
+const searchAnime = () => {
+  const url = `https://api.jikan.moe/v4/anime?q=${query.value}`
+  fetch(url).then(res => res.json()).then(res => {
+    search_results.value = res.data
+  })
+}
 </script>
 <template >
   <div>
