f0d3e46b523957cb2a7cf589389e4abfbc21fa1e
naneps <ekaprasetya2244@gmail.com>
2022-07-03 22:13:42 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref, computed, onMounted } from 'vue'
+const query = ref('');
+// const my_
+</script>
+<template >
+  <div>
+
+  </div>
+</template>
+<style lang="">
+  
+</style>
